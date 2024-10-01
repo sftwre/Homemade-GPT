256ac97979d6b66e3ee4d6f90ef38eef6f94c67e
Isaac <mightyking7@gmail.com>
2024-10-01 12:55:25 -0700

    refactored code

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from utils import load_weights_into_gpt, download_and_load_gpt2, load_dataset, t
 
 
 
-def train_model_simple(model, train_loader, val_loader,
+def train(model, train_loader, val_loader,
                        optimizer, device, num_epochs,
                        eval_freq, eval_iter, start_context,
                        tokenizer):
@@ -20,14 +20,14 @@ def train_model_simple(model, train_loader, val_loader,
             model.train()
             for input_batch, target_batch in train_loader:
                 optimizer.zero_grad()
-                loss = calc_loss_batch(input_batch, target_batch, model, device)
+                loss = batch_loss(input_batch, target_batch, model, device)
                 loss.backward()
                 optimizer.step()
                 tokens_seen += input_batch.numel()
                 global_step += 1
                 
                 if global_step % eval_freq == 0:
-                    train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                    train_loss, val_loss = evaluate(model, train_loader, val_loader, device, eval_iter)
                     train_losses.append(train_loss)
                     val_losses.append(val_loss)
                     track_tokens_seen.append(tokens_seen)
@@ -35,27 +35,27 @@ def train_model_simple(model, train_loader, val_loader,
                           f"Train loss {train_loss:.3f}, "
                           f"Val loss {val_loss:.3f}"
                          )
-            generate_and_print_sample(model, tokenizer, device, start_context)
+            print_tokens(model, tokenizer, device, start_context)
         return train_losses, val_losses, track_tokens_seen
     
-def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+def evaluate(model, train_loader, val_loader, device, eval_iter):
     model.eval()
     with torch.no_grad():
-        train_loss = calc_loss_loader(
+        train_loss = epoch_loss(
             train_loader, model, device, num_batches=eval_iter
         )
-        val_loss = calc_loss_loader(
+        val_loss = epoch_loss(
             val_loader, model, device, num_batches=eval_iter
         )
     model.train()
     return train_loss, val_loss
 
-def generate_and_print_sample(model, tokenizer, device, start_context):
+def print_tokens(model, tokenizer, device, start_context):
     model.eval()
     context_size = model.pos_emb.weight.shape[0]
     encoded = text_to_token_ids(start_context, tokenizer).to(device)
     with torch.no_grad():
-        token_ids = generate_text_simple(
+        token_ids = generate_tokens(
             model=model, idx=encoded,
             max_new_tokens=50, context_size=context_size
         )
@@ -63,7 +63,7 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
         print(decoded_text.replace("\n", " "))  # Compact print format
     model.train()
 
-def generate_text_simple(model, idx, max_new_tokens, context_size, temperature=1):
+def generate_tokens(model, idx, max_new_tokens, context_size, temperature=1):
 
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
@@ -79,7 +79,7 @@ def generate_text_simple(model, idx, max_new_tokens, context_size, temperature=1
     return idx
 
 
-def calc_loss_batch(input_batch, target_batch, model, device):
+def batch_loss(input_batch, target_batch, model, device):
     input_batch = input_batch.to(device)
     target_batch = target_batch.to(device)
     logits = model(input_batch)
@@ -87,7 +87,7 @@ def calc_loss_batch(input_batch, target_batch, model, device):
     return loss
 
 
-def calc_loss_loader(data_loader, model, device, num_batches=None):
+def epoch_loss(data_loader, model, device, num_batches=None):
     total_loss = 0.
     if len(data_loader) == 0:
         return float("nan")
@@ -97,38 +97,13 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         num_batches = min(num_batches, len(data_loader))
     for i, (input_batch, target_batch) in enumerate(data_loader):
         if i < num_batches:
-            loss = calc_loss_batch(
+            loss = batch_loss(
                 input_batch, target_batch, model, device
             )
             total_loss += loss.item()
         else:
             break
     return total_loss / num_batches
-
-
-def generate(model, idx, max_new_tokens, context_size, temperature=1.0, top_k=None, eos_id=None):
-    for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]
-        with torch.no_grad():
-            logits = model(idx_cond)
-        logits = logits[:, -1, :]
-        if top_k is not None:
-            top_logits, _ = torch.topk(logits, top_k)
-            min_val = top_logits[:, -1]
-            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device),
-                                 logits
-                                )
-            
-        if temperature > 0:
-            logits = logits / temperature
-            probs = torch.softmax(logits, dim=-1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-        else:
-            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
-        if idx_next == eos_id:
-            break
-        idx = torch.cat((idx, idx_next), dim=1)
-    return idx
 
 
 if __name__ == "__main__":
@@ -220,7 +195,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.00005, weight_decay=0.1)
     num_epochs = 2
     
-    train_losses, val_losses, tokens_seen = train_model_simple(
+    train_losses, val_losses, tokens_seen = train(
         model, train_loader, val_loader, optimizer, device,
         num_epochs=num_epochs, eval_freq=5, eval_iter=5,
         start_context=format_input(val_data[0]), tokenizer=tokenizer
