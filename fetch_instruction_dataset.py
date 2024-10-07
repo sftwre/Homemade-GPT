4036f104d174e657232671765d74e3034d64042a
Isaac <mightyking7@gmail.com>
2024-10-07 00:01:54 -0700

    added support for command line args

--- fetch_instruction_dataset.py
+++ fetch_instruction_dataset.py
@@ -1,15 +1,23 @@
 import os
 import urllib.request as request
 from urllib.error import URLError
+from argparse import ArgumentParser
 
 
 if __name__ == "__main__":
 
-    file_path = "./data/instruction-data.json"
-    url = "https://tinyurl.com/ac4jrjta"
+    parser = ArgumentParser(
+        description="Downloads an instruction dataset to a provided directory."
+    )
+    parser.add_argument("--url", type=str, help="dataset url")
+    parser.add_argument("--file_path", type=str, help="dataset file path")
+    args = parser.parse_args()
+
+    url = args.url
+    file_path = args.file_path
 
     if os.path.exists(file_path):
-         print(f"Dataset already present at: {file_path}")
+        print(f"Dataset already present at: {file_path}")
     else:
         try:
             with request.urlopen(url) as response:
@@ -19,7 +27,7 @@ if __name__ == "__main__":
                 os.makedirs(dirs, exist_ok=True)
 
                 with open(file_path, "w", encoding="utf-8") as file:
-                        file.write(text_data)
+                    file.write(text_data)
 
             print("Instruction dataset successfully downloaded!")
 
